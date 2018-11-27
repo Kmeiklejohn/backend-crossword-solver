027a544899519ce0bbf02c521761b679027a0e85
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-11-27 15:43:20 -0500

    finished

--- xword.py
+++ xword.py
@@ -5,18 +5,24 @@
 __author__ = "???"
 
 # YOUR HELPER FUNCTION GOES HERE
+import re
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
 
+    pattern = r'[a-z]'
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+        
+    replace_word = test_word.replace(' ',pattern)
+    
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
-
-
+    for word in words:
+        if re.search(replace_word, word):
+            if len(test_word) == len(word):
+                print word
+           
 if __name__ == '__main__':
     main()
